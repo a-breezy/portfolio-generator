f1117badc558712181bdb01f9ee052927bd79505
a.breezy <ambrose.wilkins@gmail.com>
2021-11-30 12:58:03 -0500

    added validation

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ const promptUser = () => {
     return inquirer.prompt([{
             type: "input",
             name: "name",
-            message: "What is your name?",
+            message: "What is your name? (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            },
         },
         {
             type: "input",
             name: "github",
-            message: "Enter yourGitHub Username",
+            message: "Enter yourGitHub Username (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your username!");
+                    return false;
+                }
+            },
         },
         {
             type: "input",
@@ -41,12 +57,28 @@ const promptProject = (portfolioData) => {
         .prompt([{
                 type: "input",
                 name: "name",
-                message: "What is the name of your project?",
+                message: "What is the name of your project? (Required)",
+                validate: (nameInput) => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter your project name!");
+                        return false;
+                    }
+                },
             },
             {
                 type: "input",
                 name: "description",
                 message: "Provide a description of the project (Required)",
+                validate: (nameInput) => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter your project description!");
+                        return false;
+                    }
+                },
             },
             {
                 type: "checkbox",
@@ -66,6 +98,14 @@ const promptProject = (portfolioData) => {
                 type: "input",
                 name: "link",
                 message: "Enter the GitHub link to your project. (Required)",
+                validate: (nameInput) => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter your project link!");
+                        return false;
+                    }
+                },
             },
             {
                 type: "confirm",
